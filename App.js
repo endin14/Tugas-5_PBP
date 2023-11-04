af08a0bd78061c9b19339e055aa4c4ac54ee7c3f
Endin Sya'dudin <130285696+endin14@users.noreply.github.com>
2023-11-04 22:34:40 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+const fullName= require('./module1');
+const myAge= require('./module2');
+const myHobby= require('./module3');
+const myWork= require('./module4');
+
+console.log(fullName('Endin syadudin'));
+console.log(myAge('20 tahun'));
+console.log(myHobby('volley,football'));
+console.log(myWork('Web developer'))
